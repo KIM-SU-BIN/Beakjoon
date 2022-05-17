a09e30007f134194d8a92ed0a9b8d4c8f6923ce2
subintoto <subintoto@naver.com>
2022-05-18 00:06:21 +0900

    Welovekriii02

---
+++ src/com/javaex/bj/Welovekriii02.java
@@ -0,0 +1,12 @@
+package com.javaex.bj;
+
+public class Welovekriii02 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");
+		
+	}
+
+}
