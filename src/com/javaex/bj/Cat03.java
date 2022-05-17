0f114141332c1bb2e63cb9babec8684654efe32b
subintoto <subintoto@naver.com>
2022-05-18 00:08:18 +0900

    cat03

---
+++ src/com/javaex/bj/Cat03.java
@@ -0,0 +1,13 @@
+package com.javaex.bj;
+
+public class Cat03 {
+
+	public static void main(String[] args) {
+	
+		System.out.println("\\    /\\\r\n"
+									+ " )  ( ')\r\n"
+									+ "(  /  )\r\n"
+									+ " \\(__)|");
+	}
+
+}
