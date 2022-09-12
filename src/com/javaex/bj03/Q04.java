31850a81a0f25aec734eab8ba1f602d14195a91d
subintoto <subintoto@naver.com>
2022-09-12 13:46:12 +0900

    백준 영수증문제

--- src/com/javaex/bj03/Q04.java
+++ src/com/javaex/bj03/Q04.java
@@ -1,11 +1,32 @@
 package com.javaex.bj03;
 
-public class Q04 {
+import java.util.Scanner;
+
+public class Q04 {		//영수증
 	
 	public static void main(String[] args){
 		
+		Scanner sc = new Scanner(System.in);
 		
+		int X = sc.nextInt();		//총금액
+		int N = sc.nextInt();		//개수
+		int sum = 0;  
 		
+		for(int i=1; i<=N; i++) {
+			
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			
+			sum += a*b; 	//sum = sum + a*b;
+		}
+		
+		if(sum == X) {
+			System.out.println("yes");
+		} else {
+			System.out.println("No");
+		}
+
+		sc.close();
 	}
 
 }
